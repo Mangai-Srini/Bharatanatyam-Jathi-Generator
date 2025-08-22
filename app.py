9b18d9f810f5e0fc368aa557e3ae10cbf9f70a8e
Srivaramangai <srivaramangai2002@gmail.com>
2025-08-22 22:58:44 +0530

    Initial commit - added README, requirements, and starter files git rebase --continue

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("🎶 Bharatanatyam Jathi Generator")
+st.write("Welcome! This app will generate rhythmic Bharatanatyam jathis using AI and Carnatic music concepts.")
